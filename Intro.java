5c1edda211cd74770ea5df79bbc1d33fe6f592a0
Isla <islarcherjr@gmail.com>
2023-10-21 09:52:33 +0100

    intro to real OOP

---
+++ Intro.java
@@ -0,0 +1,20 @@
+class Dog {
+    int size;
+    String breed;
+    String name;
+
+    // method
+    void bark() {
+        System.out.println("Ruff! Ruff!!");
+    }
+
+}
+
+public class Intro {
+    public static void main (String[] args) {
+        Dog rodger = new Dog();
+        rodger.size = 50;
+        rodger.breed = "German Shepherd";
+        rodger.bark();
+    }
+}
