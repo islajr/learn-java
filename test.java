98d3e159be04518bdc802deeadc00dbb3ac511cd
Isla <islarcherjr@gmail.com>
2023-12-12 20:42:33 +0100

    who?

--- test.java
+++ test.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class test {
 
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        int[] numbers = {2, 3, 4, 5, 6, 7};
+
+        System.out.println(Arrays.toString(numbers));
+    
     }
 }
