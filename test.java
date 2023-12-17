82b663ff1a56ac000e71a220b82a33ecfd35f589
Isla <islarcherjr@gmail.com>
2023-12-17 09:33:23 +0100

    adding notes for references

--- test.java
+++ test.java
@@ -4,12 +4,21 @@ import java.util.Arrays;
 public class test {
 
     public static void main(String[] args){
-        float cgpa = 4.21F;
-        // String[] names = {"Isaac", "Oluwaseun", "Akinmokun"};
-        int[] numbers = {2, 4, 8, 1, 10, 22};
-        System.out.println(numbers.length);
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-        
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
+
+/* Java notes */
+/* 
+ * INTRO
+ * In order to use a package, it has to be imported.
+ * java.util.Arrays is a package that takes care of arrays
+ * the String package is imported by default
+ * 
+ * DATA TYPES
+ * there are two types of data types: value types (built-ins) and reference types.
+ * any data type except int, float, double, short, long, bool, byte or char is a reference type data type and as such is not "built-in"
+ * 
+ */
