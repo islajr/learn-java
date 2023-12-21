c26bbe238776a30814f94712491fe97ecd0c570b
Isla <islarcherjr@gmail.com>
2023-12-21 23:45:07 +0100

    new example

---
+++ mortgage.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class mortgage {
+    
+    public static void main(String[] args){
+        // program to calculate the mortgage payment
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
