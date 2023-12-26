421e3af48736c1a46f1ea7f6a790a4534ab7ff54
Isla <islarcherjr@gmail.com>
2023-12-26 21:23:21 +0100

    scanner intricasies

--- BroCode/src/Main.java
+++ BroCode/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+
+        System.out.print("How old are you? ");
+        int age = input.nextInt();
+
+        // clearing the stream
+        input.nextLine();
+        System.out.print("What is your best food? ");
+        String food = input.nextLine();
+
+        System.out.println("Your name is " + name);
+        System.out.println("You are " + age + " years old.");
+        System.out.println("Your favourite food is " + food);
     }
 }
+
+/* NOTES
+* When using the scanner class to request for input, it is imperative to note that after using the .nextInt() method,
+* a new line (\n) character is left behind in the stream and as such, if it is followed by a .nextLine(), it will
+* entirely skip without requesting the input at all!
+* In order to fix this, the stream has to be flushed with a bare *inputVariable*.nextLine() before the main one.
+* */
