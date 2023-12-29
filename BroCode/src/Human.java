02f6891a64fe4f8e52698cf9f10392c0226c4464
Isla <islarcherjr@gmail.com>
2023-12-29 21:18:55 +0100

    test run for OOP

---
+++ BroCode/src/Human.java
@@ -0,0 +1,19 @@
+public class Human {
+    String name;
+    int age;
+    double height;
+
+    Human(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void eat() {
+        System.out.printf("%s is eating.\n", this.name);
+    }
+
+    void drink() {
+        System.out.printf("%s is drinking.\n", this.name);
+    }
+}
--- BroCode/src/Main.java
+++ BroCode/src/Main.java
@@ -3,36 +3,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*ArrayList<String> food = new ArrayList<String>();
-        food.add("Cupcakes");
-        food.add("Malt");
-        food.add("Pizza");
+        Human him = new Human("Isla", 20, 182);
 
-        for (int i = 0; i < food.size(); i++) {
-            System.out.println(food.get(i));
-        }*/
+        him.eat();
+        him.drink();
 
-        ArrayList<ArrayList<String>> groceryList = new ArrayList<ArrayList<String>>();
-
-        ArrayList<String> bakeryList = new ArrayList<String>();
-        bakeryList.add("Pasta");
-        bakeryList.add("Garlic Bread");
-        bakeryList.add("Doughnuts");
-
-        ArrayList<String> produceList = new ArrayList<String>();
-        produceList.add("Tomatoes");
-        produceList.add("Peppers");
-        produceList.add("Zuccini");
-
-        ArrayList<String> drinksList = new ArrayList<String>();
-        drinksList.add("Soda");
-        drinksList.add("Coffee");
-
-        groceryList.add(bakeryList);
-        groceryList.add(produceList);
-        groceryList.add(drinksList);
-
-        System.out.println(groceryList);
-        System.out.println(groceryList.get(1));
     }
 }
