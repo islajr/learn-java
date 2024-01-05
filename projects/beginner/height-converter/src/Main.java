623467c2724b97641debcb4e79098a17859e03fa
Isla <islarcherjr@gmail.com>
2024-01-05 21:13:07 +0100

    implemented working error check: pick up here tomorrow.

--- projects/beginner/height-converter/src/Main.java
+++ projects/beginner/height-converter/src/Main.java
@@ -1,15 +1,27 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         /* Program that effectively converts height from metre to ft and vice-versa */
         System.out.println("Height Converter");
-        int response;
+        int response = 0;   // random initialization to get the program to run.
         while (true) {
             System.out.println("Follow the instructions below: ");
             System.out.println("(1) - cm to ft\n(2) - ft to cm.");
             Scanner scanner = new Scanner(System.in);
-            response = scanner.nextInt();
+
+            try {
+                response = scanner.nextInt();
+            }
+            catch(NoSuchElementException e) {
+                System.out.println("That was not a valid response. ");
+                break;
+            }
+            catch (Exception e) {
+                System.out.println("Something went wrong. ");
+                break;
+            }
 
             switch (response) {
                 case 1:
