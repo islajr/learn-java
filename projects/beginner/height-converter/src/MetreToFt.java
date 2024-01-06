ce764f03c1afce3d23a62ae02e6626bbb40aa28c
Isla <islarcherjr@gmail.com>
2024-01-06 09:47:41 +0100

    implemented error-checking and recursive call for I

--- projects/beginner/height-converter/src/MetreToFt.java
+++ projects/beginner/height-converter/src/MetreToFt.java
@@ -1,11 +1,21 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class MetreToFt {
 
     static void convert() {
-        double height, raw;
+        double height = 0;
+        double raw;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Height (cm):  ");
-        height = scanner.nextInt();
+
+        try {
+            height = scanner.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("That is not valid. ");
+//            System.exit(0);
+            MetreToFt.convert();
+        }
         raw = height/30.48;
 
         int feet = (int)(raw);
