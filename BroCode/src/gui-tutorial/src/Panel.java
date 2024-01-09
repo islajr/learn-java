04237849643834f076fff2f91031f0b1ff69d072
Isla <islarcherjr@gmail.com>
2024-01-09 22:50:34 +0100

    added a second panel. will pick up later.

--- BroCode/src/gui-tutorial/src/Panel.java
+++ BroCode/src/gui-tutorial/src/Panel.java
@@ -5,18 +5,31 @@ public class Panel {
     public static void main (String[] args) {
         JFrame frame = new JFrame();
         JPanel panels = panel();
+        JPanel bluePanel = panel2();
         frame.setDefaultCloseOperation(3);
         frame.setLayout(null);
         frame.setSize(1080, 750);
         frame.setVisible(true);
         frame.add(panels);
+        frame.add(bluePanel);
     }
 
     private static JPanel panel () {
         JPanel redPanel = new JPanel();
+
         redPanel.setBackground(Color.RED);
         redPanel.setBounds(0, 0, 250, 250);
 
+
+
         return redPanel;
+    }
+
+    private static JPanel panel2 () {
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.BLUE);
+        bluePanel.setBounds(256, 0, 250, 250);
+
+        return bluePanel;
     }
 }
