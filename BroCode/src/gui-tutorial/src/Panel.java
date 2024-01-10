51319381b54a49e0fb157ed9238f9e2acfdd292f
Isla <islarcherjr@gmail.com>
2024-01-10 16:45:17 +0100

    testing adding a green panel.

--- BroCode/src/gui-tutorial/src/Panel.java
+++ BroCode/src/gui-tutorial/src/Panel.java
@@ -4,23 +4,23 @@ import java.awt.*;
 public class Panel {
     public static void main (String[] args) {
         JFrame frame = new JFrame();
-        JPanel panels = panel();
+        JPanel redPanel = panel();
         JPanel bluePanel = panel2();
-        frame.setDefaultCloseOperation(3);
+        JPanel greenPanel = panel3();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(1080, 750);
         frame.setVisible(true);
-        frame.add(panels);
+        frame.add(redPanel);
         frame.add(bluePanel);
+        frame.add(greenPanel);
     }
 
     private static JPanel panel () {
         JPanel redPanel = new JPanel();
-
         redPanel.setBackground(Color.RED);
         redPanel.setBounds(0, 0, 250, 250);
-
-
 
         return redPanel;
     }
@@ -28,8 +28,16 @@ public class Panel {
     private static JPanel panel2 () {
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.BLUE);
-        bluePanel.setBounds(256, 0, 250, 250);
+        bluePanel.setBounds(250, 0, 250, 250);
 
         return bluePanel;
+    }
+
+    private static JPanel panel3 () {
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.GREEN);
+        greenPanel.setBounds(0, 250, 500, 250);
+
+        return greenPanel;
     }
 }
