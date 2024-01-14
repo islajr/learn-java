ce8f4ab6560023d590ad876b131cb0aae9fa9797
Isla <islarcherjr@gmail.com>
2024-01-14 21:30:46 +0100

    more.

---
+++ BroCode/src/gui-tutorial/src/Flow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Flow {
+    public static void main (String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
