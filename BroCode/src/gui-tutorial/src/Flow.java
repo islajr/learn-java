53583016db4c2d2147dd04141f482a65445e461f
Isla <islarcherjr@gmail.com>
2024-01-15 15:37:25 +0100

    first look at flow layout

--- BroCode/src/gui-tutorial/src/Flow.java
+++ BroCode/src/gui-tutorial/src/Flow.java
@@ -1,10 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Flow {
     public static void main (String[] args) {
         JFrame frame = new JFrame();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(720, 720);
+        frame.setLayout(new FlowLayout());
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+        frame.add(new JButton("10"));
+
         frame.setVisible(true);
+
     }
 }
