ae8e44c3b3fa1e622f895c338af02efc9925e1a3
Isla <islarcherjr@gmail.com>
2024-01-15 21:48:44 +0100

    panels and frames with flow layout

--- BroCode/src/gui-tutorial/src/Flow.java
+++ BroCode/src/gui-tutorial/src/Flow.java
@@ -4,21 +4,27 @@ import java.awt.*;
 public class Flow {
     public static void main (String[] args) {
         JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(720, 720);
         frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
-        frame.add(new JButton("10"));
+        panel.setPreferredSize(new Dimension(250, 250));
+        panel.setBackground(Color.black);
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
 
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+        panel.add(new JButton("10"));
+
+        frame.add(panel);
         frame.setVisible(true);
 
     }
