e657311dfc3380091c4088b501dfd8691e2b421f
Isla <islarcherjr@gmail.com>
2024-01-17 20:42:51 +0100

    first working example

---
+++ BroCode/src/gui-tutorial/src/LayeredPane.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LayeredPane {
+    public static void main (String[] args) {
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.black);
+        label1.setBounds(100, 100, 500, 500);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.blue);
+        label2.setBounds(200, 200, 500, 500);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.green);
+        label3.setBounds(350, 350, 500, 500);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0,1080, 1080);
+        layeredPane.add(label1);
+        layeredPane.add(label2);
+        layeredPane.add(label3);
+
+        JFrame frame = new JFrame();
+        frame.add(layeredPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(1080, 1080);
+        frame.setVisible(true);
+    }
+}
