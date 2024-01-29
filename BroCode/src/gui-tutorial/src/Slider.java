13fbcd46f6850a9fe1764846589545e42e0e3f1d
Isla <islarcherjr@gmail.com>
2024-01-29 19:37:11 +0100

    little or no improvement plus setback.

--- BroCode/src/gui-tutorial/src/Slider.java
+++ BroCode/src/gui-tutorial/src/Slider.java
@@ -2,13 +2,21 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class Slider extends JFrame implements ChangeListener {
+public class Slider extends JFrame implements ChangeListener, KeyListener {
 
     JPanel panel;
     JLabel label;
     JSlider slider;
     JTextField field;
+
+    int sliderValue;
     Slider () {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Slider Demo");
@@ -25,16 +33,14 @@ public class Slider extends JFrame implements ChangeListener {
         slider.setFont(new Font("SF Pro Display", Font.BOLD, 15 ));
         slider.setOrientation(SwingConstants.VERTICAL);
 
-        field = new JTextField("Temperature: " + slider.getValue() + "C.");
-//        field.setText("Temperature is " + slider.getValue() + "C.");
+        sliderValue = slider.getValue();
+        field = new JTextField("Temperature: " + sliderValue + "C.");
         field.setFont(new Font("SF Pro Display", Font.BOLD, 15));
-//        field.setLayout(new FlowLayout());
-//        field.setPreferredSize(new Dimension(100, 100));
+        field.addKeyListener(this);
 
         slider.addChangeListener(this);
 
         panel.add(slider);
-//        panel.add(label);
         panel.add(field);
 
         this.add(panel);
@@ -44,5 +50,22 @@ public class Slider extends JFrame implements ChangeListener {
     @Override
     public void stateChanged(ChangeEvent changeEvent) {
         field.setText("Temperature: " + slider.getValue() + "C.");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+        if (keyEvent.equals("Backspace")) {
+            slider.setValue(sliderValue);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
     }
 }
