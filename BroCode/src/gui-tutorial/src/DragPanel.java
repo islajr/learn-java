c600cfa623c394d5483881bc49754012c72fa761
Isla <islarcherjr@gmail.com>
2024-02-06 19:30:02 +0100

    completed with errors.

--- BroCode/src/gui-tutorial/src/DragPanel.java
+++ BroCode/src/gui-tutorial/src/DragPanel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 public class DragPanel extends JPanel {
@@ -27,13 +28,22 @@ public class DragPanel extends JPanel {
     public void paintComponent(Graphics graphic) {
         // re-paints the image after the position is updated.
         super.paintComponent(graphic);
+        image.paintIcon(this, graphic, (int)imageCorner.getX(), (int)imageCorner.getY());
     }
 
     private class ClickListener extends MouseAdapter {
 
+        public void mousePressed(MouseEvent mouseEvent) {
+            prevPoint = mouseEvent.getPoint();
+        }
     }
 
     private class DragListener extends MouseMotionAdapter {
-
+        public void mouseDragged(MouseEvent mouseEvent) {
+            Point currentPoint = mouseEvent.getPoint();
+            imageCorner.translate((int)(currentPoint.getX() - prevPoint.getX()), (int)(currentPoint.getY() - prevPoint.getY()));
+            prevPoint = currentPoint;
+            repaint();
+        }
     }
 }
