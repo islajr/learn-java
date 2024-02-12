74d3c0139c8c1ca67d545c456acf2f71199ce179
Isla <islarcherjr@gmail.com>
2024-02-12 17:35:28 +0100

    all seems lost.

--- roadmap/2-getting_started/streams.java
+++ roadmap/2-getting_started/streams.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+
 public class streams {
     public static void main(String[] args) {
-        // integer stream
+        Integer[] array = new Integer[] {1, 2, 3, 4, 5};
+        List<Integer> list = Arrays.asList(array);
+
     }
 }
