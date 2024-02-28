0c91775ceae040569ff6ce13e5900863b0b85efd
Isla <islarcherjr@gmail.com>
2024-02-28 08:06:59 +0100

    resumption of official java

--- roadmap/2-getting_started/generics.java
+++ roadmap/2-getting_started/generics.java
@@ -4,8 +4,12 @@ import java.util.List;
 public class generics {
     public static void main(String[] args) {
         List<Integer> myList = new ArrayList<Integer>();
-        myList.add(23);
+        for (int i = 0; i < 9; ++i) {
+            myList.add(21 + i);
+        }
 
-        System.out.println(myList);
+
+
+        System.out.println(myList.size());
     }
 }
