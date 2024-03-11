99c20f8fccb834883c47de84c14f32010fa8f462
Isla <islarcherjr@gmail.com>
2024-03-11 14:33:14 +0100

    alternative to 'throws' in similar situation.

--- roadmap/2-getting_started/exceptions.java
+++ roadmap/2-getting_started/exceptions.java
@@ -1,6 +1,11 @@
 class exceptions {
-    public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(1000);
+    public static void main(String[] args)  {
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException i) {
+            System.out.println(i.getMessage());
+        }
         // here, this method will not run because of the possibility of the sleep being interrupted and hence, this expression needs to cater for the potential InterruptedException.
     }
 }
