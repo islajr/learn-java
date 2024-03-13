aa818666a119d7a428968a3d8643ebbcb38e418b
Isla <islarcherjr@gmail.com>
2024-03-13 14:43:04 +0100

    introduction to collection practicals and the details of the iterator interface.

---
+++ roadmap/2-getting_started/collection.java
@@ -0,0 +1,9 @@
+public class collection {
+    public static void main(String[] args) {
+        // test program for collections and their methods.
+
+        // iterator interface
+//        Iterable <T> iterator();    - runs the iterator interface over elements of type T.
+
+    }
+}
