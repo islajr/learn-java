a0915ab7510bcac5ceab903941b7f6b31c95a08f
Isla <islarcherjr@gmail.com>
2024-03-13 15:08:29 +0100

    bare lists with conventional traversing method.

--- roadmap/2-getting_started/collection.java
+++ roadmap/2-getting_started/collection.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+import java.util.*;
+
 public class collection {
     public static void main(String[] args) {
         // test program for collections and their methods.
 
         // iterator interface
 //        Iterable <T> iterator();    - runs the iterator interface over elements of type T.
+        List <Integer> ages = new ArrayList<>();
 
+        ages.add(18);
+        ages.add(21);
+        ages.add(28);
+
+        Iterator <Integer> iter = ages.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next());
+        }
     }
 }
