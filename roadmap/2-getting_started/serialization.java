601a3164073a30b9a754d2292cf0fc453fe8a470
Isla <islarcherjr@gmail.com>
2024-03-15 06:22:01 +0100

    working serialization and de-serialization process.

--- roadmap/2-getting_started/serialization.java
+++ roadmap/2-getting_started/serialization.java
@@ -1,7 +1,5 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
+
 class Demo implements Serializable {
     int a;
     String b;
@@ -23,12 +21,36 @@ public class serialization {
             ObjectOutputStream out = new ObjectOutputStream(file);  // final conversion step
 
             out.writeObject(newDemo);   // method for serialization
+
+            out.close();
             file.close();   // courtesy closing.
 
             System.out.println("Object has been serialized.");
         }
         catch (IOException ex) {
             System.out.println("Caught IOException. ");
+        }
+
+        // de-serialization process
+        Demo second = null; // creating an instance for use in the try block
+        try {
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            second = (Demo)in.readObject(); // method for de-serialization.
+
+            in.close();
+            file.close();
+
+            System.out.println("Object has been de-serialized");
+            System.out.println("a = " + second.a + ". ");
+            System.out.println("b = " + second.b + ". ");
+        }
+        catch(IOException io) {
+            System.out.println("Check your Serialization method. ");
+        }
+        catch(ClassNotFoundException cl) {
+            System.out.println("There is a problem with the UID. ");
         }
     }
 }
