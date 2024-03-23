e8d3744d2127d8b947fb266e2216da19fb897a32
Isla <islarcherjr@gmail.com>
2024-03-23 22:49:24 +0100

    added current time feature that uses an external repo.

--- roadmap/3-build-tools/test-gradle/src/main/java/hello/HelloWorld.java
+++ roadmap/3-build-tools/test-gradle/src/main/java/hello/HelloWorld.java
@@ -1,8 +1,12 @@
 package hello;
+import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Greeter greet = new Greeter();
-        System.out.println(greet.sayHello());
+       LocalTime currentTime = new LocalTime();
+       System.out.println("the current local time is " + currentTime);
+
+       Greeter greet = new Greeter();
+       System.out.println(greet.sayHello());
     }
 }
