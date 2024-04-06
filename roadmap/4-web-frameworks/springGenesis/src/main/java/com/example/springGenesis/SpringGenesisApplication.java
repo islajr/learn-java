304e80d146a56f97afd480891b0738c6593aecbd
Isla <islarcherjr@gmail.com>
2024-04-06 22:21:54 +0100

    fresh project; same problem

---
+++ roadmap/4-web-frameworks/springGenesis/src/main/java/com/example/springGenesis/SpringGenesisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springGenesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGenesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenesisApplication.class, args);
+	}
+
+}
---
+++ roadmap/4-web-frameworks/springGenesis/src/test/java/com/example/springGenesis/SpringGenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springGenesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGenesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
