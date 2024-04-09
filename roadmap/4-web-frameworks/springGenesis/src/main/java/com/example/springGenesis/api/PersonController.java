07225c0febf8be25c49eb87bd07df4526a09b462
Isla <islarcherjr@gmail.com>
2024-04-09 20:29:18 +0100

    post mapping

--- roadmap/4-web-frameworks/springGenesis/src/main/java/com/example/springGenesis/api/PersonController.java
+++ roadmap/4-web-frameworks/springGenesis/src/main/java/com/example/springGenesis/api/PersonController.java
@@ -2,15 +2,21 @@ package com.example.springGenesis.api;
 
 import com.example.springGenesis.model.Person;
 import com.example.springGenesis.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
+    @PostMapping
     public void addPerson(Person person) {
         personService.addPerson(person);
     }
