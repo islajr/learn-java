a620454f690b3e92e49e847c95974aaf5acda2cf
Isla <islarcherjr@gmail.com>
2024-04-22 20:21:27 +0100

    adding a new endpoint for getting specific photos and changing list db to a hashmap.

--- roadmap/4-web-frameworks/crash/src/main/java/com/spring/crash/CrashController.java
+++ roadmap/4-web-frameworks/crash/src/main/java/com/spring/crash/CrashController.java
@@ -1,14 +1,24 @@
 package com.spring.crash;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CrashController {
 
-    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1", "hello.jpg"));
+    }};
+
 
     @GetMapping("/")
     public String hello() {
@@ -16,7 +26,18 @@ public class CrashController {
     }
 
     @GetMapping("/photos")
-    public List<Photo> get() {
-        return db;
+    public Collection<Photo> getAllPhotos() {
+        return db.values();
+    }
+
+    @GetMapping("/photos/{id}")
+    public Photo getPhoto(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        else {
+            return photo;
+        }
     }
 }
