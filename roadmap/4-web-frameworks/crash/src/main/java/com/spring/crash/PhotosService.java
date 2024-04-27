406f76cbb65fb5f89aad4e88f2c36e546be988fb
Isla <islarcherjr@gmail.com>
2024-04-27 20:10:12 +0100

    fleshing out the methods.

--- roadmap/4-web-frameworks/crash/src/main/java/com/spring/crash/PhotosService.java
+++ roadmap/4-web-frameworks/crash/src/main/java/com/spring/crash/PhotosService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PhotosService {
@@ -14,5 +15,23 @@ public class PhotosService {
     }};
 
     public Collection<Photo> get() {
+        return db.values();
+    }
+
+    public Photo get(String id) {
+        return db.get(id);
+    }
+
+    public Photo remove(String id) {
+        return db.remove(id);
+    }
+
+    public Photo save(String filename, byte[] data) {
+        Photo photo = new Photo();
+        photo.setId(UUID.randomUUID().toString());
+        photo.setName(filename);
+        photo.setData(data);
+        db.put(photo.getId(), photo);
+        return photo;
     }
 }
