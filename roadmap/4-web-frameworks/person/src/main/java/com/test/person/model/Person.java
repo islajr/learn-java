446fca9dc4fd7fa565cf44a84545dd4d0b9a945f
Isla <islarcherjr@gmail.com>
2024-04-28 18:40:45 +0100

    defining getters and setters for the person object.

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/model/Person.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/model/Person.java
@@ -1,4 +1,30 @@
 package com.test.person.model;
 
+import java.util.UUID;
+
 public class Person {
+    private final UUID uuid;
+    private String name;
+
+
+    public Person(UUID uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+
+    }
+
+
+    // getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
 }
