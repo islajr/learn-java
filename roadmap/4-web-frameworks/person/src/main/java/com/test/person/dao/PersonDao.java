f7b0c1fa04d91f665e41f6ad9cc6906376add5d7
Isla <islarcherjr@gmail.com>
2024-04-29 18:58:38 +0100

    initial dao interface design.

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/dao/PersonDao.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/dao/PersonDao.java
@@ -1,4 +1,16 @@
 package com.test.person.dao;
+import com.test.person.model.Person;
+
+import java.util.UUID;
 
 public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    // default insertPerson() to run if the method is not overridden
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();    // a random UUID is generated and passed recursively into insertPerson()
+        return insertPerson(id, person);
+    }
+
 }
