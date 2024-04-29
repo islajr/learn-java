f95377f289824110cce57968f56d6eb9c4a7a0a6
Isla <islarcherjr@gmail.com>
2024-04-29 19:21:31 +0100

    controller initial design

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/api/PersonController.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/api/PersonController.java
@@ -1,11 +1,15 @@
 package com.test.person.api;
 
+import com.test.person.model.Person;
+import com.test.person.service.PersonService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    private PersonService personService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -15,6 +19,11 @@ public class PersonController {
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
         return "Hello there, " + name + "!";
+    }
+
+    @GetMapping("/person/create")
+    public void addPerson(Person person) {
+        personService.addPerson(person);
     }
 
 }
