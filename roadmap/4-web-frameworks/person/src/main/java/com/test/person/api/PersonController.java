802fec330b4759f3dc703e4128634a8ce14e9fd1
Isla <islarcherjr@gmail.com>
2024-04-29 18:22:33 +0100

    basic stuff 1

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/api/PersonController.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/api/PersonController.java
@@ -1,4 +1,14 @@
 package com.test.person.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello there!";
+    }
+
 }
