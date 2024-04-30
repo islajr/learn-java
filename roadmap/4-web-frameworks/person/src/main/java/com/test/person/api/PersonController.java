9e7a5b32a09b7606b5506d0f36462e572e7dd867
Isla <islarcherjr@gmail.com>
2024-04-30 18:15:47 +0100

    first attempt at creating a person.

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/api/PersonController.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/api/PersonController.java
@@ -2,9 +2,7 @@ package com.test.person.api;
 
 import com.test.person.model.Person;
 import com.test.person.service.PersonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PersonController {
@@ -21,8 +19,8 @@ public class PersonController {
         return "Hello there, " + name + "!";
     }
 
-    @GetMapping("/person/create")
-    public void addPerson(Person person) {
+    @PostMapping("/person/create")
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 
