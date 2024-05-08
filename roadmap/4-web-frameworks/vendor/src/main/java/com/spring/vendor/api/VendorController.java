2227e3ffec63209a342c02fb498fbba754924d26
Isla <islarcherjr@gmail.com>
2024-05-08 23:25:32 +0100

    greens 1

--- roadmap/4-web-frameworks/vendor/src/main/java/com/spring/vendor/api/VendorController.java
+++ roadmap/4-web-frameworks/vendor/src/main/java/com/spring/vendor/api/VendorController.java
@@ -1,9 +1,13 @@
 package com.spring.vendor.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VendorController {
 
-    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
 }
