c52396acd95197c65a0a4d2a736e5da42fe0be54
Isla <islarcherjr@gmail.com>
2024-05-09 19:40:54 +0100

    tweaked the rest controller class to accurately reflect the tutorial.

--- roadmap/4-web-frameworks/vendor/src/main/java/com/spring/vendor/api/VendorController.java
+++ roadmap/4-web-frameworks/vendor/src/main/java/com/spring/vendor/api/VendorController.java
@@ -1,13 +1,17 @@
 package com.spring.vendor.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.vendor.model.CloudVendor;
+
 @RestController
+@RequestMapping("/cloudvendor")
 public class VendorController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello, world!\nWelcome to Cloud Vendor";
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+        return new CloudVendor("c1", "vendor 1", "address 1", "xxxxx");
     }
 }
