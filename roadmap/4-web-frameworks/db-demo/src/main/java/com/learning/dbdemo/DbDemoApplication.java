7f9c46487895e4b12a5b6cb4376931a58b0f09ce
Isla <islarcherjr@gmail.com>
2024-05-12 22:04:45 +0100

    the main controller? idk what i'm doing.

--- roadmap/4-web-frameworks/db-demo/src/main/java/com/learning/dbdemo/DbDemoApplication.java
+++ roadmap/4-web-frameworks/db-demo/src/main/java/com/learning/dbdemo/DbDemoApplication.java
@@ -1,13 +1,28 @@
 package com.learning.dbdemo;
 
+import com.learning.dbdemo.entity.StoreInformation;
+import com.learning.dbdemo.repository.StoreInformationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbDemoApplication {
+public class DbDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	StoreInformationRepository storeInformationRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		StoreInformation storeInformation = new StoreInformation("Amazon", "All", "12345");
+		storeInformationRepository.save(storeInformation);
+
+		storeInformationRepository.findByStoreName("Amazon").forEach(System.out::println);
+	}
 }
