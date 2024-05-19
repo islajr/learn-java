1c1efa2df53ed9585da5c514243d78d351ee89f6
Isla <islarcherjr@gmail.com>
2024-05-19 21:00:59 +0100

    added controller advice annotation with some comments.

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/exception/PersonExceptionHandler.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/exception/PersonExceptionHandler.java
@@ -2,12 +2,14 @@ package com.test.person.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice   //annotation to handle multiple exceptions globally
 public class PersonExceptionHandler {
 
     // method to handle the PersonNotFoundException
-    @ExceptionHandler(value = {PersonNotFoundException.class})
+    @ExceptionHandler(value = {PersonNotFoundException.class})  // annotation to handle specific exception
     public ResponseEntity<Object> handlePersonNotFoundException(PersonNotFoundException personNotFoundException) {
         PersonException personException = new PersonException(
                 personNotFoundException.getMessage(),
