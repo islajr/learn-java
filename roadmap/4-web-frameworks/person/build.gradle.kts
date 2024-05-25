15b20a76f65f62f2c7ecc948c3b1d552f3b76f01
Isla <islarcherjr@gmail.com>
2024-05-25 23:19:45 +0100

    added lombok integration

--- roadmap/4-web-frameworks/person/build.gradle.kts
+++ roadmap/4-web-frameworks/person/build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flyway:flywaydb:flyway-core")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
