a16f2addd2a5db523138ad70492811016a837bf7
Isla <islarcherjr@gmail.com>
2024-06-04 16:18:00 +0100

    commented out lombok and flyway dependencies.

--- roadmap/4-web-frameworks/person/build.gradle.kts
+++ roadmap/4-web-frameworks/person/build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.flyway:flywaydb:flyway-core")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+//	implementation("org.flyway:flywaydb:flyway-core")
+//	compileOnly("org.projectlombok:lombok")
+//	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
