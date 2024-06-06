8a70233c339cd3dd621e6a613aacc2272f69d142
Isla <islarcherjr@gmail.com>
2024-06-06 20:02:14 +0100

    completing the data access layer

--- roadmap/4-web-frameworks/person/src/main/java/com/test/person/dao/PersonRepository.java
+++ roadmap/4-web-frameworks/person/src/main/java/com/test/person/dao/PersonRepository.java
@@ -1,4 +1,12 @@
 package com.test.person.dao;
 
-public class PersonRepository {
+import com.test.person.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, UUID> {
+
 }
