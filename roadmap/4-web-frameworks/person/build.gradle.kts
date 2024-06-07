94c557145f75ccf15bf918630139159a467866ee
Isla <islarcherjr@gmail.com>
2024-06-07 23:05:14 +0100

    re-added flyway for database migrations.

--- roadmap/4-web-frameworks/person/build.gradle.kts
+++ roadmap/4-web-frameworks/person/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("org.flyway:flywaydb:flyway-core")
+	implementation("org.flyway:flywaydb:flyway-core")
 //	compileOnly("org.projectlombok:lombok")
 //	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
