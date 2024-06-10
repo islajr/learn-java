fa43d445df74091231a7832ea152d92a2be3eb18
Isla <islarcherjr@gmail.com>
2024-06-10 19:38:24 +0100

    student class with constructors, getters and setters.

---
+++ roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/Student.java
@@ -0,0 +1,80 @@
+package com.example.student.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+    private LocalDate date;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(long id, String name, int age, LocalDate date, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.date = date;
+        this.email = email;
+    }
+
+    public Student(String name, int age, LocalDate date, String email) {
+        this.name = name;
+        this.age = age;
+        this.date = date;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", date=" + date +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
