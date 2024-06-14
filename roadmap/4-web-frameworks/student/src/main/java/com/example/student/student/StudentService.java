a5a9849105d111676aad1de3556e283be9d58c7d
Isla <islarcherjr@gmail.com>
2024-06-14 04:43:09 +0100

    implementing a jpa method via dependency injection

--- roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentService.java
+++ roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.student.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,15 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        394,
-                        "Isla",
-                        21,
-                        LocalDate.of(2003, Month.MAY, 1),
-                        "islarcherjr@gmail.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
