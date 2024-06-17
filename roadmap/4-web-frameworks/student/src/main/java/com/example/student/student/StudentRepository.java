08662d0ca2866451f2750874d72fda8597ca2936
Isla <islarcherjr@gmail.com>
2024-06-17 20:58:05 +0100

    another way to check for email with jpql??

--- roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentRepository.java
+++ roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.example.student.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    // SELECT * FROM student WHERE email = ?
+    @Query("SELECT s from Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
