7d3b3091656efce07042459f548ff913a0b1e399
Isla <islarcherjr@gmail.com>
2024-06-18 23:59:21 +0100

    first steps toward the delete student function.

--- roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentController.java
+++ roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentController.java
@@ -25,4 +25,9 @@ public class StudentController {
     public void registerStudent(@RequestBody Student student) {
         studentService.addStudent(student);
     }
+
+    @DeleteMapping(path = "/delete/{studentID}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
 }
