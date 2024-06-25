5db76a9fb029927d8b22e171cfde13bef40261cd
Isla <islarcherjr@gmail.com>
2024-06-25 22:19:38 +0100

    starter files for a to-do list project.

---
+++ roadmap/4-web-frameworks/todo-list/src/test/java/com/archer/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archer.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
