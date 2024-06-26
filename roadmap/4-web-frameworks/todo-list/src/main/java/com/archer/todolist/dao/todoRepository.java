f1d4b4dc26145c48d9902af2b11852c42b2bedfd
Isla <islarcherjr@gmail.com>
2024-06-26 19:36:55 +0100

    creation of a repository to harness the power of spring jpa

---
+++ roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/dao/todoRepository.java
@@ -0,0 +1,10 @@
+package com.archer.todolist.dao;
+
+import com.archer.todolist.model.todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface todoRepository extends JpaRepository<todo, Integer> {
+
+}
