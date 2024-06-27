0212d78b3d9a7d1e058e8b08e726dc13b56f1c31
Isla <islarcherjr@gmail.com>
2024-06-27 23:03:31 +0100

    working post method.

--- roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/service/todoService.java
+++ roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/service/todoService.java
@@ -1,11 +1,21 @@
 package com.archer.todolist.service;
 
 import com.archer.todolist.model.todo;
+import com.archer.todolist.dao.todoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class todoService {
-    public void createItem(todo todo) {
 
+    private final todoRepository repository;
+
+    @Autowired
+    public todoService(todoRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createItem(todo todo) {
+        repository.save(todo);
     }
 }
