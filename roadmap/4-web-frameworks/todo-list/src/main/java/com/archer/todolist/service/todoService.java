a6d7ea1cb9b64201392e4eaf866587d42b433248
Isla <islarcherjr@gmail.com>
2024-06-27 22:53:54 +0100

    bare service file.

---
+++ roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/service/todoService.java
@@ -0,0 +1,11 @@
+package com.archer.todolist.service;
+
+import com.archer.todolist.model.todo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class todoService {
+    public void createItem(todo todo) {
+
+    }
+}
