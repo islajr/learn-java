1a746c1cab0458790c2bcca3cc7f6c3387ae8cfd
Isla <islarcherjr@gmail.com>
2024-06-27 22:54:51 +0100

    post mapping.

--- roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/api/todoController.java
+++ roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/api/todoController.java
@@ -1,6 +1,7 @@
 package com.archer.todolist.api;
 
 import com.archer.todolist.model.todo;
+import com.archer.todolist.service.todoService;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/todo")
 public class todoController {
 
-    @PostMapping("/create")
-    public void createItem() {
+    private final todoService _todoservice;
 
+    public todoController(todoService service) {
+        this._todoservice = service;
+    }
+
+    @PostMapping("/create")
+    public void createItem(@RequestBody todo todo) {
+        _todoservice.createItem(todo);
     }
 
 }
