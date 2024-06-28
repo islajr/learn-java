9f4e47bfa66a61ea2ab6f572780344667fc36ed6
Isla <islarcherjr@gmail.com>
2024-06-28 15:27:04 +0100

    added new constructor.

--- roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/model/todo.java
+++ roadmap/4-web-frameworks/todo-list/src/main/java/com/archer/todolist/model/todo.java
@@ -38,6 +38,11 @@ public class todo {
         this.name = name;
     }
 
+    public todo(String name, LocalDateTime timeIssued) {
+        this.name = name;
+        this.timeIssued = timeIssued;
+    }
+
     public todo() {
 
     }
