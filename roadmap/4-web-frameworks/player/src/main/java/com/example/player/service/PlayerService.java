36b7250ad90e6f93662c26eddc665aaece827f7d
Isla <islarcherjr@gmail.com>
2024-07-17 09:40:28 +0100

    bare unfleshed out implementations of player service

---
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/service/PlayerService.java
@@ -0,0 +1,22 @@
+package com.example.player.service;
+
+import com.example.player.model.Player;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PlayerService {
+
+    public Optional<Player> getPlayers() {
+        return null;
+    }
+
+    public void createPlayer(Player player) {
+    }
+
+    public Optional<Player> getPlayer(UUID id) {
+        return null;
+    }
+}
