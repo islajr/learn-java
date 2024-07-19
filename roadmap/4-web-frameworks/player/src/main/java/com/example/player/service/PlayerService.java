d58b2e91fe6f7abe52b4517f5c3b5d3080e59a08
Isla <islarcherjr@gmail.com>
2024-07-20 00:58:11 +0100

    dependency injection of repository

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/service/PlayerService.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/service/PlayerService.java
@@ -1,6 +1,8 @@
 package com.example.player.service;
 
 import com.example.player.model.Player;
+import com.example.player.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -8,6 +10,13 @@ import java.util.UUID;
 
 @Service
 public class PlayerService {
+
+    public final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
 
     public Optional<Player> getPlayers() {
         return null;
