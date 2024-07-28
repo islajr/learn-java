73cc5cc89956b4dbdd9e407f0f997371cbcafd27
Isla <islarcherjr@gmail.com>
2024-07-28 23:12:44 +0100

    fixed the repository errors also.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/PlayerRepository.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/PlayerRepository.java
@@ -1,8 +1,9 @@
 package com.example.player.repository;
 
 import com.example.player.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayerRepository extends Repository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 }
