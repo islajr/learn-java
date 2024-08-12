a99c45aa1387e76e4ca1641d7339a41ac82ab855
Isla <islarcherjr@gmail.com>
2024-08-12 22:31:20 +0100

    hit a bit of a snag.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/security/SpringSecurityAuthentication.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/security/SpringSecurityAuthentication.java
@@ -2,14 +2,27 @@ package com.example.player.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityAuthentication {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(
+                (req) -> req
+                        .requestMatchers("/api/v1/player/getPlayer/", "/api/v1/player/createPlayer").permitAll()
+                        .anyRequest().authenticated()
+        )   ;
+
+        return http.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
