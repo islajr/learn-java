fc2b7181da0029683e1db16820818d3020b74f21
Isla <islarcherjr@gmail.com>
2024-08-15 23:20:49 +0100

    beginnings of new implementation.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/model/Player.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/model/Player.java
@@ -1,10 +1,15 @@
 package com.example.player.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table
-public class Player {
+public class Player implements UserDetails {
 
     @Id
     @SequenceGenerator(
@@ -64,4 +69,38 @@ public class Player {
         this.age = age;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
