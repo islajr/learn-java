95982779bc205ed08597d0af3dfa40273bc04a32
Isla <islarcherjr@gmail.com>
2024-08-26 22:07:30 +0100

    basic security filter to support web form and postman.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.player.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 }
