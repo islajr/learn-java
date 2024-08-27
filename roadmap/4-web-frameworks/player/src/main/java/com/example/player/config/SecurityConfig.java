b25e6d2ff175bb5a0edb3449a37632a765794ed7
Isla <islarcherjr@gmail.com>
2024-08-27 21:17:13 +0100

    commenting and making http stateless

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,10 +15,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        // disabling csrf
         http.csrf(customizer -> customizer.disable());
+
+        //making web server able to authorize http requests
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+
+        // initiating form login
         http.formLogin(Customizer.withDefaults());
+
+        // making it accessible to postman
         http.httpBasic(Customizer.withDefaults());
+
+        // making csrf stateless
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
