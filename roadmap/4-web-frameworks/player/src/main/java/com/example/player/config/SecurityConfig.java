03644bd141aaf1fa2fe72df1de71ac7d9eb01414
Isla <islarcherjr@gmail.com>
2024-09-01 22:16:31 +0100

    slight change.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,7 +18,7 @@ public class SecurityConfig {
 
         http
         // disabling csrf
-                .csrf(customizer -> customizer.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
         //making web server able to authorize http requests
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
