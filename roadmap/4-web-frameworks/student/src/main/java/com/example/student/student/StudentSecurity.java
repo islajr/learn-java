771a328658355b42ea0474e76d8ada62821d50a6
Isla <islarcherjr@gmail.com>
2024-09-02 21:56:15 +0100

    first steps towards spring security

---
+++ roadmap/4-web-frameworks/student/src/main/java/com/example/student/student/StudentSecurity.java
@@ -0,0 +1,18 @@
+package com.example.student.student;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class StudentSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+
+        return http.build();
+    }
+}
