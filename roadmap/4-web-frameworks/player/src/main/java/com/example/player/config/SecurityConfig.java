5ff911150ba3e6d06af1cf1d5a0ce82b3c7a064a
Isla <islarcherjr@gmail.com>
2024-09-08 23:59:03 +0100

    userdetails method to add new users and roles in customized format.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -35,6 +39,17 @@ public class SecurityConfig {
 
         return http.build();
 
+    }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("archer")
+                .password("archerslaps")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
     }
 }
