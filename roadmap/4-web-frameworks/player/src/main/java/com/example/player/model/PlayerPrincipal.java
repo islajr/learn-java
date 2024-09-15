f068ff9d8a03059e49aa91be76dc3d6665c906c6
Isla <islarcherjr@gmail.com>
2024-09-15 23:55:10 +0100

    class to implement userdetails.

---
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/model/PlayerPrincipal.java
@@ -0,0 +1,53 @@
+package com.example.player.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PlayerPrincipal implements UserDetails {
+
+    private Player player;
+
+    public PlayerPrincipal(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return player.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
