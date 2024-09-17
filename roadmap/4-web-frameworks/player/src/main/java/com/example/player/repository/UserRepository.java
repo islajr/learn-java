08f2ab3e25e8ff623e793b30577cc706138c910c
Isla <islarcherjr@gmail.com>
2024-09-17 21:27:10 +0100

    slight addition of tag

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/UserRepository.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.player.repository;
 
 import com.example.player.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
 }
