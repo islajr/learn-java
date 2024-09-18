a649c5104e31815a9886de8d7193ad2f7e5f478d
Isla <islarcherjr@gmail.com>
2024-09-18 22:37:33 +0100

    linked UserRepository to UserDetailsService.

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/UserRepository.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.player.repository;
 
+import com.example.player.model.Player;
 import com.example.player.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
+    Users findByUsername(String username);
 }
