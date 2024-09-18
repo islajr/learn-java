72ca3e62d044a9feae2e660e86a5bf6472b03921
Isla <islarcherjr@gmail.com>
2024-09-18 22:36:08 +0100

    import and return userPrincipal instance

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/service/MyUserDetailsService.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/service/MyUserDetailsService.java
@@ -1,31 +1,31 @@
 package com.example.player.service;
 
+import com.example.player.model.Users;
+import com.example.player.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.example.player.model.Player;
-import com.example.player.model.PlayerPrincipal;
-import com.example.player.repository.PlayerRepository;
+import com.example.player.model.UserPrincipal;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {   
 
     @Autowired
-    private PlayerRepository playerRepository;
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Player player = playerRepository.findByUsername(username);              
+        Users users = userRepository.findByUsername(username);
 
-        if (player == null) {
+        if (users == null) {
             System.out.println("User not found!");
             throw new UsernameNotFoundException("User not found!");
         }
 
-        return new PlayerPrincipal(player);
+        return new UserPrincipal(users);
     }
 }
