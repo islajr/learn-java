2fa3504f2b911c673f7ac57bc5fed0b9a7cd5b0b
Isla <islarcherjr@gmail.com>
2024-09-18 22:37:07 +0100

    edited and returned PlayerRepository to original form

--- roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/PlayerRepository.java
+++ roadmap/4-web-frameworks/player/src/main/java/com/example/player/repository/PlayerRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
-    Player findByUsername(String username);
 }
