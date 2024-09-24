098f3c2015e43c4489ec4dacba5892753c4bdb99
Isla <islarcherjr@gmail.com>
2024-09-24 22:31:21 +0100

    for today

---
+++ roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/security/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.security_demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+
+}
