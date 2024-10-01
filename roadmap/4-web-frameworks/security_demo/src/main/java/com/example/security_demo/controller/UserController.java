03256809dfdcdc284de39075df7e20397304ff9f
Isla <islarcherjr@gmail.com>
2024-10-01 22:33:57 +0100

    redirect loop error

--- roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/controller/UserController.java
+++ roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.security_demo.controller;
 
+import com.example.security_demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class UserController {
     @GetMapping("/security/greet")
     public String greet() {
         return "<h1>Hello, Stranger! </h1>";
+    }
+
+    @GetMapping("/security/users")
+    public String getUser() {
+        return "This is a user!";
     }
 }
