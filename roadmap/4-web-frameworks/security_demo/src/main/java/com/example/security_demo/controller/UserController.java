d5aa83427bded52a97b3fcd70488d2cd6c6e615a
Isla <islarcherjr@gmail.com>
2024-10-02 22:37:56 +0100

    csrf

--- roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/controller/UserController.java
+++ roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.security_demo.controller;
 
 import com.example.security_demo.model.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,10 @@ public class UserController {
     @GetMapping("/security/users")
     public String getUser() {
         return "This is a user!";
+    }
+
+    @GetMapping("/security/csrf")
+    public CsrfToken getCsrf(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 }
