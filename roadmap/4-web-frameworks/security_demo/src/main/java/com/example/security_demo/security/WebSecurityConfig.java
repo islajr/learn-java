491edad9c6bb227efa71df9ddfe7e70e36f31c56
Isla <islarcherjr@gmail.com>
2024-10-09 21:31:30 +0100

    bypassing the password encoding check

--- roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/security/WebSecurityConfig.java
+++ roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/security/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package com.example.security_demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,7 +18,7 @@ public class WebSecurityConfig {
     @Bean
    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-         http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/security/greet").permitAll() // permit requests to this URL
                 .anyRequest().authenticated()   // any other request needs to be authenticated
@@ -28,9 +30,17 @@ public class WebSecurityConfig {
                 .logout(LogoutConfigurer::permitAll);*/
 
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
 
+   }
+
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("Isla")
+                .roles("ADMIN")
+                .password("{noop}password");
    }
 }
