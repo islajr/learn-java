ec0120dbc7f65ac6a0cf76f97ed029740dd83690
Isla <islarcherjr@gmail.com>
2024-10-17 23:34:54 +0100

    request mapping implementation + repository integration

--- roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/controller/UserController.java
+++ roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/controller/UserController.java
@@ -7,28 +7,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping(path = "/security")
 @RestController
 public class UserController {
 
-    @Autowired
-    private UsersService usersService;
+    private final UsersService usersService;
 
-    @GetMapping("/security/greet")
+    @Autowired
+    public UserController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/greet")
     public String greet() {
         return "<h1>Hello, Stranger! </h1>";
     }
 
-    @GetMapping("/security/users")
+    @GetMapping("/users")
     public String getUser() {
         return "This is a user!";
     }
 
-    @GetMapping("/security/csrf")
+    @GetMapping("/csrf")
     public CsrfToken getCsrf(HttpServletRequest request) {
         return (CsrfToken) request.getAttribute("_csrf");
     }
 
-    @PostMapping("/security/createUser")
+    @PostMapping("/createUser")
     public void createUser(@RequestBody User user) {
         UsersService.createUser(user);
     }
--- roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/service/UsersService.java
+++ roadmap/4-web-frameworks/security_demo/src/main/java/com/example/security_demo/service/UsersService.java
@@ -1,11 +1,18 @@
 package com.example.security_demo.service;
 
 import com.example.security_demo.model.User;
+import com.example.security_demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
-    public void createUser(User user) {
+    private final UserRepository userRepository;
 
+    public UsersService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void createUser(User user) {
+        userRepository.save(user);
     }
 }
