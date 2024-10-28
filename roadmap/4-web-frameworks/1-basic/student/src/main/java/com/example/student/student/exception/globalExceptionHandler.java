7cc57db030711c5a1fe2174b78bc3c81b75338bb
Isla <islarcherjr@gmail.com>
2024-10-28 09:02:44 +0100

    handler for studentAlreadyExistsException

---
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/exception/globalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.student.student.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class globalExceptionHandler {
+
+    @ExceptionHandler(value = studentDoesNotExistException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody ErrorResponse handleException(studentDoesNotExistException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    }
+
+    
+    @ExceptionHandler(value = studentAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public @ResponseBody ErrorResponse handleException(studentAlreadyExistsException ex) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
+    }
+
+}
