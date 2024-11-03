32ee8494ce66a39e42d316476df1bf8c9de0c1fe
Isla <islarcherjr@gmail.com>
2024-11-03 23:57:45 +0100

    defining the security filter class

---
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
@@ -0,0 +1,9 @@
+package com.example.student.student.security;
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class securityConfig extends WebSecurityConfigurer {
+
+}
