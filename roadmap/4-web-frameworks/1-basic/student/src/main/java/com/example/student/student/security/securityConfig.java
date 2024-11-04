07dfde00d7410bce1dcaeed654a84211055b3759
Isla <islarcherjr@gmail.com>
2024-11-04 23:50:47 +0100

    prototyping the methods.

--- roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
@@ -1,9 +1,19 @@
 package com.example.student.student.security;
 
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 public class securityConfig extends WebSecurityConfigurer {
 
+    @Override
+    public void init(SecurityBuilder builder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+
+    }
 }
