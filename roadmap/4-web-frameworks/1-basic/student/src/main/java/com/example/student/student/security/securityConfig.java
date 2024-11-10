90ea278b76e408b658459e8a0b8346f25af1e60f
Isla <islarcherjr@gmail.com>
2024-11-10 20:23:10 +0100

    implemented users and selective role-based endpoint access.

--- roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -40,6 +41,26 @@ public class securityConfig {
                 .logout(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails first = User
+                .withDefaultPasswordEncoder()
+                .username("isla")
+                .password("isla1234")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails second = User
+                .withDefaultPasswordEncoder()
+                .username("emma")
+                .password("emma1234")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(first, second);
     }
 
 }
