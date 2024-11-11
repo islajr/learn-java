56b11fc3ea548e5049325d0d629bd9b0a944e939
Isla <islarcherjr@gmail.com>
2024-11-11 23:48:15 +0100

    created a UserDetails implementation to link to UserDetailsService.

---
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/model/userPrincipal.java
@@ -0,0 +1,52 @@
+package com.example.student.student.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class userPrincipal implements UserDetails {
+
+    private user _user;
+
+    public userPrincipal(user _user) {
+        this._user = _user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return _user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return _user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
