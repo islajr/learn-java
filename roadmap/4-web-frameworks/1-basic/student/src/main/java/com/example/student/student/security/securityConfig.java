c59ac42ac4ff30aaed836b685fad2ea63df68e93
Isla <islarcherjr@gmail.com>
2024-11-21 20:11:36 +0100

    added authentication manager

--- roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/security/securityConfig.java
@@ -3,9 +3,11 @@ package com.example.student.student.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -53,6 +55,11 @@ public class securityConfig {
         provider.setUserDetailsService(userDetailsService);
 
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
