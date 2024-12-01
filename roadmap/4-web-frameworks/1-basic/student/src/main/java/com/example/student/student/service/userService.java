d5edf44115250e0fb7d6d443ffd4e8bf6522d77d
Isla <islarcherjr@gmail.com>
2024-12-01 16:20:43 +0100

    basic steps towards user verification; failing though.

--- roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/service/userService.java
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/service/userService.java
@@ -3,6 +3,9 @@ package com.example.student.student.service;
 import com.example.student.student.model.user;
 import com.example.student.student.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,28 @@ public class userService {
 
     @Autowired
     private UserRepository userRepository;
-    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public String registerUser(user _user) {
         _user.setPassword(encoder.encode(_user.getPassword()));
         userRepository.save(_user);
-        return "Successfully registered new user \"" + _user.getUsername() + "\"    .";
+        return "Successfully registered new user \"" + _user.getUsername() + "\".";
+    }
+
+    public String verify (user _user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(_user.getUsername(), _user.getPassword()));
+
+        if (authentication.isAuthenticated()) {
+            System.out.println("Successfully Authenticated");
+            return "Successfully Authenticated!";
+        }
+
+        System.out.println("Authentication Failed");
+        return "Authentication Failed";
     }
 }
