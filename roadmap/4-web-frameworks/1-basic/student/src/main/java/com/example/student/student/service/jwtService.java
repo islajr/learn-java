7ee0d11eecd48d1fd8fb5bbdfa249e7102e8a913
Isla <islarcherjr@gmail.com>
2024-12-02 23:57:34 +0100

    generating a proper key in the constructor that passes all checks.

--- roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/service/jwtService.java
+++ roadmap/4-web-frameworks/1-basic/student/src/main/java/com/example/student/student/service/jwtService.java
@@ -5,13 +5,29 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class jwtService {
+
+    final String secret;
+
+    public jwtService() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGenerator.generateKey();
+            secret = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String generateToken(String username) {
 
@@ -27,7 +43,6 @@ public class jwtService {
     }
 
     private Key generateKey() {
-        final String secret = "elkfnve23knsa";
         byte[] keyByte = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyByte);
     }
