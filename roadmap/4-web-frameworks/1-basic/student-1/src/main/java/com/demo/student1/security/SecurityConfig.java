0357263db2677d743c13fadde8a64849e0fc7b03
Isla <islarcherjr@gmail.com>
2024-12-24 23:47:50 +0100

    adding spring security dependency addition

---
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.demo.student1.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
