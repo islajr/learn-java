9f5dc8b2b75611553da64dcfb3d5391c1de71e00
Isla <islarcherjr@gmail.com>
2024-12-27 00:00:32 +0100

    basic get and post mapping with correct setting for spring security dependency 1.1

--- roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.demo.student1.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 }
