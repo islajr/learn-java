ac78761afa7ec8fa1cf4aa0a46e4f180bec4a57e
Isla <islarcherjr@gmail.com>
2024-12-27 11:05:05 +0100

    basic authentication implementation through the use of a filter chain

--- roadmap/4-web-frameworks/1-basic/student-1/build.gradle.kts
+++ roadmap/4-web-frameworks/1-basic/student-1/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.security:spring-security-web")
-//	implementation("org.springframework.boot:spring-boot-starter-security:3.4.1")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.4.1")
 
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
@@ -1,7 +1,25 @@
 package com.demo.student1.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
 }
