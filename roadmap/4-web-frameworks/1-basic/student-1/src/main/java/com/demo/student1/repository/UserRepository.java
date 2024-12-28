69ce1a67a5c944e2726694fec84d09eb1c927690
Isla <islarcherjr@gmail.com>
2024-12-28 22:39:23 +0100

    defined user repository

---
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.demo.student1.repository;
+
+import com.demo.student1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
