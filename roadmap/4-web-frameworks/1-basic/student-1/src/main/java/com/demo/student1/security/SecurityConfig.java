8903787fad79a98b9b1da7d338639f5f91dd5601
Isla <islarcherjr@gmail.com>
2024-12-29 22:22:07 +0100

    tried to implement authentication provider methods.

--- roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.demo.student1.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,6 +31,16 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+
+    }
+
+    @Bean
+    protected AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authentication = new DaoAuthenticationProvider();
+        authentication.setUserDetailsService(userDetailsService);
+
+        return authentication;
 
     }
 }
