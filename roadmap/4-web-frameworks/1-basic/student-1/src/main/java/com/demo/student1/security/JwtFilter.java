d74ba02605c694c0c8b78545cffb43dab7e2886e
Isla <islarcherjr@gmail.com>
2025-01-09 21:02:20 +0100

    first steps at fleshing out the jwt filter.

--- roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/JwtFilter.java
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/security/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.demo.student1.security;
 
+import com.demo.student1.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,8 +12,23 @@ import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+
+    private final String BEARER_PREFIX = "Bearer ";
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
 
+        if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
+            token = authHeader.substring(BEARER_PREFIX.length());
+            username = jwtService.extractUsername(authHeader);
+        }
     }
 }
