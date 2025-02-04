1d03fc03b287106040b354dd9bc2a46ba537b97a
Isla <islarcherjr@gmail.com>
2025-02-04 23:05:34 +0100

    changed models to auto-generate id.

--- roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/model/Student.java
+++ roadmap/4-web-frameworks/1-basic/student-1/src/main/java/com/demo/student1/model/Student.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
