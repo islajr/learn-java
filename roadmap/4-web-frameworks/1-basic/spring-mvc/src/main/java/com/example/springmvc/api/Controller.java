4c7daec187c294d986a31fdad20333ba5a0abd81
Isla <islarcherjr@gmail.com>
2025-02-06 23:50:52 +0100

    done with spring mvc till further notice.

---
+++ roadmap/4-web-frameworks/1-basic/spring-mvc/src/main/java/com/example/springmvc/api/Controller.java
@@ -0,0 +1,13 @@
+package com.example.springmvc.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
