0ea77a0897aecc3c297487de42ff994c7883d1f2
Isla <islarcherjr@gmail.com>
2025-02-16 22:28:53 +0100

    first look at main launcher.

--- projects/beginner/task-tracker/src/Main.java
+++ projects/beginner/task-tracker/src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        switch (args[1]) {
+            case "add":
+                
+                break;
+            
+            case "update":
+                
+                break;
+
+            case "delete":
+
+                break;
+
+            case "list":
+
+                break;
+
+            case "mark-in-progress":
+
+                break;
+        
+            case "mark-done":
+                
+                break;
+
+            default:
+            
+                break;
+        }
+
     }
 }
