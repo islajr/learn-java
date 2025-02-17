56d9a8a180bbe9e81b0a5be10efab2f34e4d3156
Isla <islarcherjr@gmail.com>
2025-02-17 23:23:45 +0100

    drafted out some major functions.

--- projects/beginner/task-tracker/src/TaskManager.java
+++ projects/beginner/task-tracker/src/TaskManager.java
@@ -1,3 +1,34 @@
+
+import java.util.ArrayList;
+
 public class TaskManager {
+
+    public void addTask(String subject) {
+        
+    }
+
+    public void updateTask(String subject) {
+
+    }
+
+    public void deleteTask(int id) {
+
+    }
+
+    public ArrayList<Task> listAll() {
+        return null;
+    }
+
+    public ArrayList<Task> listByStatus(String status) {
+        return null;
+    }
+
+    public void markInProgress(int id) {
+
+    }
+
+    public void markDone(int id) {
+        
+    }
 
 }
