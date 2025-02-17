5516594bcc4ae24789f550d19011273f955dd413
Isla <islarcherjr@gmail.com>
2025-02-17 08:02:20 +0100

    changed Main.java to TaskApp.java and changed legacy switch case to new syntax

---
+++ projects/beginner/task-tracker/src/TaskApp.java
@@ -0,0 +1,32 @@
+public class TaskApp {
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.println("Usage: t");
+        }
+        
+        switch (args[1]) {
+            case "add" -> {
+            }
+            
+            case "update" -> {
+            }
+
+            case "delete" -> {
+            }
+
+            case "list" -> {
+            }
+
+            case "mark-in-progress" -> {
+            }
+        
+            case "mark-done" -> {
+            }
+
+            default -> {
+            }
+        }
+
+    }
+}
