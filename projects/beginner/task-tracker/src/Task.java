59ee18fbc856925db365c6f8ed97a350cf065374
Isla <islarcherjr@gmail.com>
2025-02-17 22:53:20 +0100

    added time tracking and parsed accoringly.

--- projects/beginner/task-tracker/src/Task.java
+++ projects/beginner/task-tracker/src/Task.java
@@ -1,16 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Task {
 
-    private Long id;
+    private final int id;
     public String subject;
     public String status;
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
 
     public Task(String subject) {
-        this.id = id;
+        this.id = 1;
         this.subject = subject;
-        this.status = "to do";
+        this.status = "todo";
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
@@ -20,15 +27,25 @@ public class Task {
 
     public void updateSubject(String newSubject) {
         status = newSubject;
+        updatedAt = LocalDateTime.now();
     }
 
     public void markDone() {
         status = "done";
+        updatedAt = LocalDateTime.now();
     }
+
+    public String parseTime(LocalDateTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-mm-yyyy hh:mm:ss");
+        return time.format(formatter);
+    }
+
 
     @Override
     public String toString() {
-        return "id: " + id + " subject: " + subject + " status: " + status + ".";
+        return "id: " + id + " subject: " + subject + " status: " + status + " createdAt: " 
+        + parseTime(createdAt) + " last updated: " + parseTime(updatedAt) + ".";
     }
 
+    
 }
