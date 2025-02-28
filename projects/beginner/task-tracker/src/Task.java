6ed7fcda1831e861ae9d9a768106a2a26569df4a
Isla <islarcherjr@gmail.com>
2025-02-28 23:32:39 +0100

    successfully split dateTime string

--- projects/beginner/task-tracker/src/Task.java
+++ projects/beginner/task-tracker/src/Task.java
@@ -20,6 +20,8 @@ public class Task {
         
     }
 
+    static DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
     public int getId() {
         return id;
     }
@@ -48,32 +50,32 @@ public class Task {
     }
 
     public String parseTime(LocalDateTime time) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-mm-yyyy hh:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
         return time.format(formatter);
     }
 
     public String toJson() {
         return "{\"id\": " + id + ", \"subject\": \"" + subject + "\", \"status\": \"" +  status + "\", \"createdAt\": \"" + 
-        parseTime(createdAt) + "\", \"lastUpdated\": \"" + parseTime(updatedAt) + "\"}";
+        createdAt + "\", \"lastUpdated\": \"" + updatedAt + "\"}";
     }
 
-    public Task fromJson(String jsonTask) {
+    public static Task fromJson(String jsonTask) {
 
-        jsonTask = jsonTask.replace("{", "").replace("}", "").replace(",", "");
+        jsonTask = jsonTask.replace("{", "").replace("}", "");
         String[] jsonArray = jsonTask.split(",");
 
         String id = jsonArray[0].split(":")[1].strip();
         String subject = jsonArray[1].split(":")[1].strip();
         String status = jsonArray[2].split(":")[1].strip();
-        String createdAt = jsonArray[3].split("[a-z]:")[1].strip();
-        String lastUpdated = jsonArray[4].split("[a-z]:")[1].strip();
+        String createdAt = jsonArray[3].split(": ")[1].strip();
+        String lastUpdated = jsonArray[4].split(": ")[1].strip();
 
         Task task = new Task(subject);
 
         task.id = Integer.parseInt(id);
         task.status = status;
-        task.createdAt = LocalDateTime.parse(createdAt);
-        task.updatedAt = LocalDateTime.parse(lastUpdated);
+        task.createdAt = parseTime(createdAt);
+        task.updatedAt = LocalDateTime.parse(lastUpdated, formatter);
 
         return task;
     }
@@ -82,7 +84,7 @@ public class Task {
     @Override
     public String toString() {
         return "id: " + id + " subject: " + subject + " status: " + status + " createdAt: " 
-        + parseTime(createdAt) + " lastUpdated: " + parseTime(updatedAt) + ".";
+        + createdAt.format(formatter) + " lastUpdated: " + updatedAt.format(formatter) + ".";
     }
 
     
