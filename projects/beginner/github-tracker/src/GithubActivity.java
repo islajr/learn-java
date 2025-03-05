0a3f71a95430e94398123d53f57bd2458c793222
Isla <islarcherjr@gmail.com>
2025-03-05 23:35:51 +0100

    project setup.

--- projects/beginner/github-tracker/src/GithubActivity.java
+++ projects/beginner/github-tracker/src/GithubActivity.java
@@ -1,6 +1,14 @@
 public class GithubActivity {
-
+    public static ActivityTracker tracker = new ActivityTracker();
     public static void main(String[] args) {
-
+        if (args.length < 1)
+            System.out.println("Usage GithubActivity <username>");
+        else {
+            try {
+                tracker.track(args[0]);
+            } catch (Exception e) {
+                System.out.println("I'll tend to you later");
+            }
+        }
     }
 }
