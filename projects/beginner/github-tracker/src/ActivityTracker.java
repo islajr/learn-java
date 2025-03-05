6252cd674d0afb1b18444b1ed8861fffe69effb3
Isla <islarcherjr@gmail.com>
2025-03-05 23:36:08 +0100

    project setup for main business logic - will continue tomorrow.

---
+++ projects/beginner/github-tracker/src/ActivityTracker.java
@@ -0,0 +1,11 @@
+import java.util.Optional;
+
+public class ActivityTracker {
+
+    private static final String GITHUB_API_URL = "";
+
+    public Optional<String> track(String username) {
+
+    }
+
+}
