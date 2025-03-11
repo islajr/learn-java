8df910138a10ff08854e61bc500c9d84c393e20c
Isla <islarcherjr@gmail.com>
2025-03-11 22:27:10 +0100

    been a little while: added comments.

--- projects/beginner/github-tracker/src/ActivityTracker.java
+++ projects/beginner/github-tracker/src/ActivityTracker.java
@@ -12,8 +12,21 @@ public class ActivityTracker {
         HttpClient httpClient = HttpClient.newHttpClient();
 
         try {
+
+            // sending and storing request in a response object
             HttpRequest request = HttpRequest.newBuilder().uri(new URI(GITHUB_URL)).header("Accept", "application/vnd.github+json").GET().build();
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // action statements
+            if (response.statusCode() == 404) {
+                System.out.println("User not found!");
+                return;
+            } else if (response.statusCode() == 200) {
+                // use gson repo to deal with json.
+            } else {
+                System.out.println("Error " + response.statusCode() + "!");
+            }
+
 
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
