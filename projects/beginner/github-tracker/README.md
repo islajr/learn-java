2f9f3021cd4df289a9001b906da4d0260021fe52
Isla <islarcherjr@gmail.com>
2025-03-14 22:19:15 +0100

    re-wrote previous code in new setting.

--- projects/beginner/github-tracker/README.md
+++
@@ -1,3 +0,0 @@
-# Github Activity Tracker
-
-This is an application that uses the Github API to display the recent activity of a specified user.
--- projects/beginner/github-tracker/src/main/java/org/example/GithubTracker.java
+++ projects/beginner/github-tracker/src/main/java/org/example/GithubTracker.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -5,32 +7,32 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ActivityTracker {
+public class GithubTracker {
+    public static void main(String[] args) {
 
-    public void fetchData(String username) {
-        String GITHUB_URL = "https://api.github.com/users" + username + "/events";
+        if (args.length != 1) {
+            System.out.println("Usage: GithubTracker <username>");
+        }
+        else {
+            try {
+                fetchData(args[0]);
+
+            } catch (Exception e) {
+                System.out.println("Please, input a proper username!");
+            }
+        }
+    }
+
+    private static void fetchData(String username) {
+        String GITHUB_URL = "https://api.github.com/users/" + username + "/events";
         HttpClient httpClient = HttpClient.newHttpClient();
 
         try {
-
-            // sending and storing request in a response object
             HttpRequest request = HttpRequest.newBuilder().uri(new URI(GITHUB_URL)).header("Accept", "application/vnd.github+json").GET().build();
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-            // action statements
-            if (response.statusCode() == 404) {
-                System.out.println("User not found!");
-                return;
-            } else if (response.statusCode() == 200) {
-                // use gson repo to deal with json.
-            } else {
-                System.out.println("Error " + response.statusCode() + "!");
-            }
-
 
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
