f5b153ceb06fc9eab4d24b653a578f29b0b83776
Isla <islarcherjr@gmail.com>
2025-03-15 23:34:36 +0100

    started using gson library to implement parsing methods.

--- projects/beginner/github-tracker/src/main/java/org/example/GithubTracker.java
+++ projects/beginner/github-tracker/src/main/java/org/example/GithubTracker.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -31,8 +34,20 @@ public class GithubTracker {
             HttpRequest request = HttpRequest.newBuilder().uri(new URI(GITHUB_URL)).header("Accept", "application/vnd.github+json").GET().build();
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
+            if (response.statusCode() == 404) {
+                System.out.println("There is no such user!");
+            } else if (response.statusCode() == 200) {
+                JsonArray jsonArray = JsonParser.parseString(response.body()).getAsJsonArray();
+                displayData(jsonArray);
+            } else {
+                System.out.println("Something went wrong!");
+            }
+
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void displayData(JsonArray jsonArray) {
     }
 }
