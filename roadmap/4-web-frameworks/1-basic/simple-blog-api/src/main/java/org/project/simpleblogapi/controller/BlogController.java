2279f86de89c94015a23a1252e88baf5b9ee3a7f
Isla <islarcherjr@gmail.com>
2025-03-18 21:41:34 +0100

    fleshed out controller class.

--- roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/controller/BlogController.java
+++ roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/controller/BlogController.java
@@ -1,9 +1,42 @@
 package org.project.simpleblogapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.project.simpleblogapi.model.BlogPost;
+import org.project.simpleblogapi.service.BlogService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BlogController {
 
+    private final BlogService blogService;
+
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
+    @PostMapping()
+    public BlogPost createPost(@RequestBody BlogPost) {
+        return blogService.createPost();
+    }
+
+    @GetMapping()
+    public List<BlogPost> getPosts() {
+        return blogService.getPosts();
+    }
+
+    @GetMapping("/{id}")
+    public BlogPost getPost(@PathVariable Long id) {
+        return blogService.getPost();
+    }
+
+    @PutMapping("/{id}")
+    public BlogPost updatePost(@PathVariable Long id) {
+        return blogService.updatePost();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        blogService.deletePost();
+    }
 }
--- roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/service/BlogService.java
+++ roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/service/BlogService.java
@@ -1,7 +1,27 @@
 package org.project.simpleblogapi.service;
 
+import org.project.simpleblogapi.model.BlogPost;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlogService {
+
+    public BlogPost createPost() {
+
+    }
+
+    public List<BlogPost> getPosts() {
+    }
+
+    public BlogPost getPost() {
+        return null;
+    }
+
+    public BlogPost updatePost() {
+    }
+
+    public void deletePost() {
+    }
 }
