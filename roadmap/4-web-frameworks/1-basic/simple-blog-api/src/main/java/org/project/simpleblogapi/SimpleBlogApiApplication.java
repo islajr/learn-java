7d950309c4d608f1fc3df197fff38bb581263b4d
Isla <islarcherjr@gmail.com>
2025-03-18 10:48:11 +0100

    started a simple blog api.

---
+++ roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/SimpleBlogApiApplication.java
@@ -0,0 +1,13 @@
+package org.project.simpleblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBlogApiApplication.class, args);
+    }
+
+}
---
+++ roadmap/4-web-frameworks/1-basic/simple-blog-api/src/test/java/org/project/simpleblogapi/SimpleBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.simpleblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
