fdba7cea8e13720bff8bf8224d760be480f01cd7
Isla <islarcherjr@gmail.com>
2025-03-21 13:57:02 +0100

    completed token generation method.

--- roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/service/JwtService.java
+++ roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/service/JwtService.java
@@ -1,9 +1,30 @@
 package org.project.simpleblogapi.service;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
+
+    public JwtService() {
+    }
+
     public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        Key secretKey = Jwts.SIG.HS256.key().build();
+
+        return Jwts.builder()
+                .subject(username)
+                .claims()
+                .add(claims)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() * 60 * 60 * 1000))
+                .and()
+                .signWith(secretKey).compact();
     }
 }
