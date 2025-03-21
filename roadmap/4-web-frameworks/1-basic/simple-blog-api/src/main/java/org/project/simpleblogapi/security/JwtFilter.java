010a577e1f9f1f7816aa33d0d0b908b62b886d19
Isla <islarcherjr@gmail.com>
2025-03-21 16:29:41 +0100

    enabled jwtfilter

--- roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/security/JwtFilter.java
+++ roadmap/4-web-frameworks/1-basic/simple-blog-api/src/main/java/org/project/simpleblogapi/security/JwtFilter.java
@@ -6,16 +6,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.project.simpleblogapi.service.CustomUserDetailsService;
 import org.project.simpleblogapi.service.JwtService;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Configuration
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
