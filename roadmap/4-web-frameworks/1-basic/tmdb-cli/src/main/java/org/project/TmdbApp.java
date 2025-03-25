106d486760366514d5d5d1166eb09e9a9f991519
Isla <islarcherjr@gmail.com>
2025-03-25 10:05:27 +0100

    completed "/upcoming" and some minor fixes.

--- roadmap/4-web-frameworks/1-basic/tmdb-cli/src/main/java/org/project/TmdbApp.java
+++ roadmap/4-web-frameworks/1-basic/tmdb-cli/src/main/java/org/project/TmdbApp.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,6 +11,8 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TmdbApp {
     public static void main(String[] args) {
@@ -96,7 +97,29 @@ public class TmdbApp {
                 }
 
                 case "upcoming" -> {
+                    try {
+                        JsonArray jsonResponse = fetchResponse("/upcoming");
 
+                        for (JsonElement element : jsonResponse) {
+                            JsonArray resultsArray = element.getAsJsonObject().get("results").getAsJsonArray();
+
+                            for (JsonElement result : resultsArray) {
+                                JsonObject resultObject = result.getAsJsonObject();
+                                String releaseDate = resultObject.get("release_date").getAsString();
+                                LocalDate releaseLocalDate = LocalDate.parse(releaseDate, DateTimeFormatter.ISO_DATE);
+
+                                if (releaseLocalDate.isAfter(LocalDate.now())) {
+                                    String name = resultObject.get("title").getAsString();
+                                    int popularity = resultObject.get("popularity").getAsInt();
+
+                                    System.out.println("Name: "+ name + "\nRelease Date: " + releaseDate + "\nPopularity: " + popularity + "\nVote Count: " + "\n\n");
+                                }
+                            }
+                        }
+
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
 
                 default -> {}
@@ -109,11 +132,12 @@ public class TmdbApp {
 
     private static JsonArray fetchResponse(String endpoint) throws URISyntaxException {
         final String TMDB_API_URL = "https://api.themoviedb.org/3/movie" + endpoint;
+        final String API_READ_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2Q4ZjQ4NjY1ZWQxNGVjNDdhYzczNWU0NjkwNzgzOSIsIm5iZiI6MTc0Mjg1NTk4NS43NTUsInN1YiI6IjY3ZTFkZjMxNGM1Mjc0NjY2NWRjNjQ2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ch5kfd1m9V1EQu6qRlNhpEjWzgj-lBS63BDn5bqUtB0";
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request =  HttpRequest.newBuilder()
                 .uri(new URI(TMDB_API_URL))
                 .GET()
-                .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2Q4ZjQ4NjY1ZWQxNGVjNDdhYzczNWU0NjkwNzgzOSIsIm5iZiI6MTc0Mjg1NTk4NS43NTUsInN1YiI6IjY3ZTFkZjMxNGM1Mjc0NjY2NWRjNjQ2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ch5kfd1m9V1EQu6qRlNhpEjWzgj-lBS63BDn5bqUtB0")
+                .header("Authorization", "Bearer " + API_READ_TOKEN)
                 .header("Accept", "application/json").build();
 
         try {
