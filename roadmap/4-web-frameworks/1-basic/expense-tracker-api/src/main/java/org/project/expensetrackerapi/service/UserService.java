5f4f1a2c967fcb203d4eb7f21b172637ecb9c122
Isla <islarcherjr@gmail.com>
2025-03-28 18:53:30 +0100

    added user controllers and respective services.

--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/UserService.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/UserService.java
@@ -1,4 +1,14 @@
 package org.project.expensetrackerapi.service;
 
+import org.project.expensetrackerapi.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public String login(User user) {
+
+    }
+
+    public String register(User user) {
+    }
 }
