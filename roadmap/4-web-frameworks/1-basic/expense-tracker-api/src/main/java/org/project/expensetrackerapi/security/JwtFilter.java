3f7b863948cea5815ba4e8b1d512bd2d4fe198ab
Isla <islarcherjr@gmail.com>
2025-03-28 20:48:58 +0100

    added jwtFilter.

--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/security/JwtFilter.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/security/JwtFilter.java
@@ -4,15 +4,47 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.project.expensetrackerapi.service.CustomUserDetailsService;
+import org.project.expensetrackerapi.service.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@AllArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = null;
+        String authHeader = request.getHeader("Authorization");
+        String username = null;
+        final String AUTH_PREFIX = "Bearer ";
 
+        if (authHeader != null && authHeader.startsWith(AUTH_PREFIX)) {
+            token = authHeader.substring(AUTH_PREFIX.length());
+            username = jwtService.extractUsername(token);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken upAuthToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                upAuthToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(upAuthToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/JwtService.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/JwtService.java
@@ -1,10 +1,20 @@
 package org.project.expensetrackerapi.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class JwtService {
+
     public String generateToken(String username) {
 
+    }
+
+    public String extractUsername(String token) {
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
     }
 }
