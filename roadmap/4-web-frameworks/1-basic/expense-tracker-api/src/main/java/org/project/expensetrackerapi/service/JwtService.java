3a3dc6434d09435f6d580c5fcbd3178e4b9808bd
Isla <islarcherjr@gmail.com>
2025-03-28 22:35:42 +0100

    untested: completed jwtService.

--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/JwtService.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/JwtService.java
@@ -1,20 +1,62 @@
 package org.project.expensetrackerapi.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
 public class JwtService {
 
     public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .subject(username)
+                .claims()
+                .add(claims)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + (10 * 60 * 1000)))
+                .and()
+                .signWith(generateKey())
+                .compact();
 
+    }
+
+    private SecretKey generateKey() {
+        return Jwts.SIG.HS256.key().build();
+    }
+
+    private Claims extractClaims(String token) {
+        try {
+           return Jwts.parser()
+                    .verifyWith(generateKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (JwtException e) {
+            throw new RuntimeException("This is not a valid JWT token");
+        }
     }
 
     public String extractUsername(String token) {
+        Claims claims = extractClaims(token);
+        return claims.getSubject();
     }
 
     public boolean validateToken(String token, UserDetails userDetails) {
+        return (Objects.equals(extractUsername(token), userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
     }
 }
