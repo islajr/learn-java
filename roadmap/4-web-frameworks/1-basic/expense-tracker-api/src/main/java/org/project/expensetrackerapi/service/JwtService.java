bc6a7a9bf7a351d61b6eed498fd439e4f0d1a63d
Isla <islarcherjr@gmail.com>
2025-03-28 20:33:50 +0100

    completed /login and /register logic.

---
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/JwtService.java
@@ -0,0 +1,10 @@
+package org.project.expensetrackerapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String generateToken(String username) {
+
+    }
+}
--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/UserService.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/service/UserService.java
@@ -1,14 +1,44 @@
 package org.project.expensetrackerapi.service;
 
+import lombok.AllArgsConstructor;
 import org.project.expensetrackerapi.model.User;
+import org.project.expensetrackerapi.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
     public String login(User user) {
 
+        if (user != null) {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+            if (authentication.isAuthenticated()) {
+                return jwtService.generateToken(user.getUsername());
+            }
+
+            throw new RuntimeException("Authentication Failed!");
+        }
+
+        throw new RuntimeException("User is null.");
     }
 
     public String register(User user) {
+
+        if (user != null) {
+            user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+            userRepository.save(user);
+            return "Successfully registered new user " + user.getUsername() + ".";
+        }
+
+        throw new RuntimeException("User is null.");
     }
 }
