216817f1cb7d0c586132d5a51036497a4f36563d
Isla <islarcherjr@icloud.com>
2025-04-04 21:55:49 +0100

    changed expense entity

--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/model/Expense.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/model/Expense.java
@@ -1,11 +1,6 @@
 package org.project.expensetrackerapi.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -25,8 +20,13 @@ public class Expense {
     @Column(name = "username")
     private String username;
 
-    @Column(name = "category")
+    @ManyToOne
+    @JoinColumn(name = "category")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private User user;
 
     @Column(name = "cost")
     private double cost;
