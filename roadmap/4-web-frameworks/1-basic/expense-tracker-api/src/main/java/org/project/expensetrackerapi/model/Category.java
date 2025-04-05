16bd843351c4e9b5dbbf2b53738c9f82b0b4ad0f
Isla <islarcherjr@icloud.com>
2025-04-05 22:32:39 +0100

    changed category entity back to enum

--- roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/model/Category.java
+++ roadmap/4-web-frameworks/1-basic/expense-tracker-api/src/main/java/org/project/expensetrackerapi/model/Category.java
@@ -2,21 +2,29 @@ package org.project.expensetrackerapi.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-public class Category {
-    /*Groceries,
+/*@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity*/
+public enum Category {
+    Groceries,
     Leisure,
     Electronics,
     Utilities,
     Clothing,
     Health,
-    Others*/
+    Others
 
-    @Id
+    /*@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -28,5 +36,5 @@ public class Category {
     private User user;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
-    private List<Expense> expenses = new ArrayList<>();
+    private List<Expense> expenses = new ArrayList<>();*/
 }
