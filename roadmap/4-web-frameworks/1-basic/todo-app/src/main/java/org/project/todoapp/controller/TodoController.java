de83426b03723d2489b02d7f8414d19f1cec7682
Isla <islarcherjr@icloud.com>
2025-04-19 21:40:07 +0100

    fixed stupid error.

--- roadmap/4-web-frameworks/1-basic/todo-app/src/main/java/org/project/todoapp/controller/TodoController.java
+++ roadmap/4-web-frameworks/1-basic/todo-app/src/main/java/org/project/todoapp/controller/TodoController.java
@@ -3,6 +3,7 @@ package org.project.todoapp.controller;
 import lombok.AllArgsConstructor;
 import org.project.todoapp.model.Todo;
 import org.project.todoapp.repository.TodoRepository;
+import org.project.todoapp.service.TodoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,20 +12,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/todo")
 public class TodoController {
 
-    private final TodoRepository todoRepository;
+    private final TodoService todoService;
 
     @PostMapping("")
     public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
-        return todoRepository.createTodo(todo);
+        return todoService.createTodo(todo);
     }
 
     @PutMapping("")
     public ResponseEntity<Todo> updateTodo(@RequestParam Long id, @RequestBody Todo todo) {
-        return todoRepository.updateTodo(id, todo);
+        return todoService.updateTodo(id, todo);
     }
 
     @DeleteMapping("")
     public ResponseStatus deleteTodo(@RequestParam Long id) {
-        return todoRepository.deleteTodo(id);
+        return todoService.deleteTodo(id);
     }
 }
