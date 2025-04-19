9fa2ea366f387f22fa847185272428dceed7c1b3
Isla <islarcherjr@icloud.com>
2025-04-19 21:37:31 +0100

    corrected user source url.

--- roadmap/4-web-frameworks/1-basic/todo-app/src/main/java/org/project/todoapp/controller/UserController.java
+++ roadmap/4-web-frameworks/1-basic/todo-app/src/main/java/org/project/todoapp/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("todo/api/user")
+@RequestMapping("api/todo/user")
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class UserController {
