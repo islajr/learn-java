f7e176bc92719416760adf537f7df490cbbd7526
Isla <islarcherjr@icloud.com>
2025-04-28 20:48:59 +0100

    untested: completed simple rate-limiter that switches between user and ip.

--- roadmap/4-web-frameworks/1-basic/todo-app/src/main/java/org/project/todoapp/security/RateLimiter.java
+++ roadmap/4-web-frameworks/1-basic/todo-app/src/main/java/org/project/todoapp/security/RateLimiter.java
@@ -7,16 +7,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.project.todoapp.model.UserPrincipal;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class RateLimiter extends OncePerRequestFilter {
-
-    private final Bucket bucket;
 
     /*
     * Bandwidth type variable - limit - .classic() sets the capacity of requests per minute to 10, and the refill value to ten tokens per minute with the Refill.
@@ -27,21 +30,28 @@ public class RateLimiter extends OncePerRequestFilter {
     * bucket4j also allows us to set multiple limits to enable finer-grain access control of the rate limits.
     * */
 
-    public RateLimiter() {
-        Bandwidth limit = Bandwidth.classic(10, Refill.intervally(10, Duration.ofMinutes(1)));
+        Bandwidth limit = Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1)));
 
-        this.bucket = Bucket.builder()
-                .addLimit(limit)
-                .build();
-    }
+        Map<String, Bucket> storage = new ConcurrentHashMap<>();
+
+        private Bucket createBucket() {
+            return Bucket.builder()
+                    .addLimit(limit)
+                    .build();
+        }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String validator = ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail() != null ? ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail() : request.getRemoteAddr();
+
+        Bucket bucket = storage.computeIfAbsent(validator, b -> createBucket());
+
         if (bucket.tryConsume(1)) {
             filterChain.doFilter(request, response);
         } else {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);   // should be TOO MANY REQUESTS
-            response.getWriter().write("Too many requests - try again later.");
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());   // should be TOO MANY REQUESTS
+            response.getWriter().write("You are currently rate-limited - try again later.");
         }
     }
 }
