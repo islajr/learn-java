75ae05dbde3eb67fea2b75766a96b48995ec1357
Isla <islarcherjr@icloud.com>
2025-05-05 23:16:22 +0100

    none

---
+++ roadmap/4-web-frameworks/1-basic/todo-app/src/test/java/org/project/todoapp/repository/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package org.project.todoapp.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+}
