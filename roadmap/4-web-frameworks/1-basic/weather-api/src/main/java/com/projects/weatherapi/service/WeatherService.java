24be7d119019058f251e1012dc09a6f52ac4d758
Isla <islarcherjr@icloud.com>
2025-05-11 23:37:53 +0100

    first commit just to detail what i am doing.

--- roadmap/4-web-frameworks/1-basic/weather-api/src/main/java/com/projects/weatherapi/service/WeatherService.java
+++ roadmap/4-web-frameworks/1-basic/weather-api/src/main/java/com/projects/weatherapi/service/WeatherService.java
@@ -1,11 +1,51 @@
 package com.projects.weatherapi.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @Service
 public class WeatherService {
+        @Value("${weather.api.url}") String baseURL;
+        @Value("${weather.api.key}") private String key;
+
     public ResponseEntity<String> getWeather() {
-        return ResponseEntity.ok("Works!");
+
+        // input sanitation
+
+        // connection to third-party api
+        HttpResponse<String> response = sendRequest();
+
+        // requests and caching
+
+        // response filtering and display
+
+    }
+
+    private HttpResponse<String> sendRequest() {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request;
+
+        try {
+            request = HttpRequest.newBuilder()
+                    .uri(new URI(baseURL + ))
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
